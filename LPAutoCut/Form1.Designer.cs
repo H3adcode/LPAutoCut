ea03e954b552d647937b52d173d125200b335760
H3adcode <michael.middelhoff@outlook.com>
2015-10-23 16:45:41 +0200

    copyright

--- LPAutoCut/Form1.Designer.cs
+++ LPAutoCut/Form1.Designer.cs
@@ -52,6 +52,7 @@
             this.bt_start = new System.Windows.Forms.Button();
             this.bt_save = new System.Windows.Forms.Button();
             this.bt_load = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.groupBox4.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -372,11 +373,22 @@
             this.bt_load.UseVisualStyleBackColor = true;
             this.bt_load.Click += new System.EventHandler(this.bt_load_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(448, 329);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "©2015 Michael Middelhoff";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(586, 343);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.bt_load);
             this.Controls.Add(this.bt_save);
             this.Controls.Add(this.bt_export);
@@ -393,6 +405,7 @@
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -427,6 +440,7 @@
         private System.Windows.Forms.Button bt_load;
         private System.Windows.Forms.Button bt_restoreDefault;
         private System.Windows.Forms.Button bt_setDefault;
+        private System.Windows.Forms.Label label3;
 
     }
 }
