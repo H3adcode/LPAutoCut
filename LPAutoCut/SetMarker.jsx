bc66b91fdf81a113bfc396d3d3807b2c4875a362
Michael Middelhoff <michael.middelhoff@outlook.com>
2015-10-28 00:21:24 +0100

    jsx

---
+++ LPAutoCut/SetMarker.jsx
@@ -0,0 +1,13 @@
+﻿#target premierepro
+var b = File(Folder.temp + "\\LPAutoCut.temp.txt");
+b.open('r');
+var line = "";
+var markerInfo;
+m = app.project.activeSequence.markers;
+while(!b.eof) {
+	line = b.readln();
+	markerInfo = line.split(" ");
+	var marker = m.createMarker(parseInt(markerInfo[0]));
+	marker.name = markerInfo[1];
+}
+b.close();
